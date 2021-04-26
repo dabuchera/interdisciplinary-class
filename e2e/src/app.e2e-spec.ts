06c64a785532d21e66b371fefd1761c2bae98d10
David Bucher <bucher@ibi.baug.ethz.ch>
2021-04-26 22:00:31 +0200

    new 2

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('bim-leaning app is running!');
+    expect(page.getTitleText()).toEqual('interdisciplinary-class app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bim-lean'`, () => {
+  it(`should have as title 'interdisciplinary-class'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('bim-lean');
+    expect(app.title).toEqual('interdisciplinary-class');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('bim-lean app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('interdisciplinary-class app is running!');
   });
 });
